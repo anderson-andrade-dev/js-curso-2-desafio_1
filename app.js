88a07b2b163cb2260b89aa7ac93a50d1f3176abf
Anderson Andrade <147995158+anderson-andrade-dev@users.noreply.github.com>
2024-06-20 11:26:11 -0300

    Excercicios do segundo curso One Oracle

---
+++ app.js
@@ -0,0 +1,36 @@
+/*
+Faça o download de outro projeto clicando neste link e abra no Visual Studio Code.
+
+Altere o conteúdo da tag h1 com document.querySelector e atribua o seguinte texto: Hora do Desafio.
+
+Crie uma função que exiba no console a mensagem O botão foi clicado sempre que o botão Console for pressionado.
+
+Crie uma função que exiba um alerta com a mensagem: Eu amo JS, sempre que o botão Alerta for pressionado.
+
+Crie uma função que é executada quando o botão prompt é clicado, perguntando o nome de uma cidade do Brasil. Em seguida, exiba um alerta com a mensagem concatenando a resposta com o texto: Estive em {cidade} e lembrei de você.
+
+Ao clicar no botão soma, peça 2 números inteiros e exiba o resultado da soma em um alerta.
+*/
+
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hoa do Desafio';
+
+function botaoClicado() {
+    console.log('O botão foi clicado')
+}
+
+function mensagem() {
+    alert('Eu amo JS');
+}
+
+function cidade() {
+    let cidade = prompt('Qual o nome da sua cidade? ');
+    alert(`Estive em ${cidade} e lembrei de você`)
+}
+
+function soma() {
+    let valor1 = parseInt(prompt('Adicione o primerio valor: '));
+    let valor2 = parseInt(prompt('Adicione o segundo valor: '));
+    let resultado = valor1 + valor2
+    alert(`A soma dos valores são: ${resultado}`);
+}
